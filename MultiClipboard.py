39d3f3a659f9102a062a51d16ddb4e701fd15664
FountainOfGib <dfo4242@gmail.com>
2022-01-27 00:01:03 +0000

    added arguments

--- MultiClipboard.py
+++ MultiClipboard.py
@@ -2,6 +2,24 @@ import sys
 import clipboard
 import json
 
+from enum import Enum
+class modeEnum(Enum):
+    DINGUS = 0
+    SAVE = 1
+    PASTE = 2
+    LIST = 3
 
+mode = modeEnum.DINGUS
 
+if len(sys.argv) == 2:
+    cmnd = sys.argv[1]
+
+    if cmnd[0] in ['s', 'S', 'save', 'Save']:
+        mode = modeEnum.SAVE
+    elif cmnd[0] in ['p', 'P', 'paste', 'Paste']:
+        mode = modeEnum.PASTE
+    elif cmnd[0] in ['l', 'L', 'list', 'List']:
+        mode = modeEnum.LIST
+
+if mode is modeEnum.DINGUS: raise ValueError("Not a valid mode [save, paste, list]")
 print("helloWorld ya dingus")
